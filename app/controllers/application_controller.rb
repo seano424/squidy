4bf1b287f9a0d92e325888736cb1121c5cd67bc9
Sean OReilly <soreilly424@gmail.com>
2020-12-21 15:04:49 -0500

    added sanitized params for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :profile_photo) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :profile_photo) }
+  end
 end
