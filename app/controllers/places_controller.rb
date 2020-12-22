7f2aaa5fea1ad454ac840ab3c1277e8e6ddf25f9
Sean OReilly <soreilly424@gmail.com>
2020-12-22 14:04:37 -0500

    map added through geocode

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,18 @@
 class PlacesController < ApplicationController
-  def index; end
+  def index
+    @places = Place.all
+
+    @markers = @places.geocoded.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { place: place }),
+        image_url: helpers.asset_url('location.png')
+      }
+    end
+  end
+
   def show
+
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address? && latitude.nil?
+  after_validation :geocode, if: :will_save_change_to_address? && :latitude.nil?
   acts_as_votable
 end
