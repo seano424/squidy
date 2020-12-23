d058b8733cdc0414ed5b7cfebac53eb30e3bea9c
Diana Pinon <dianapinonb@gmail.com>
2020-12-23 11:20:38 -0700

    Created reviews controller, its actions and views

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address? && :latitude?
   acts_as_votable
+  has_many :reviews
 
   def latitude?
     self.latitude.nil?
