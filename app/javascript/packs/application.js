1b186c9e9f93f27431ec934dec559f314df7ae9d
Sean OReilly <soreilly424@gmail.com>
2020-12-27 08:33:36 -0500

    added some js and css to the reviews tab

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 import { searchMap } from '../plugins/search_mapbox';
+import { collapseBtn } from '../plugins/collapse';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
   searchMap();
+  collapseBtn();
 });
---
+++ app/javascript/plugins/collapse.js
@@ -0,0 +1,27 @@
+const collapseBtn = () => {
+  const collapseBtn = document.querySelector('.collapse-text');
+  const findOutMoreBtn = document.querySelector('.find-out-more');
+  const truncatedInfo = document.querySelector('.truncate-me');
+
+  collapseBtn.addEventListener('click', () => {
+    console.log(collapseBtn.innerHTML);
+    if (collapseBtn.innerHTML === "See more") {
+      collapseBtn.innerHTML = "See less"
+    } else {
+      collapseBtn.innerHTML = "See more"
+    }
+  })
+
+  findOutMoreBtn.addEventListener('click', () => {
+    console.log(truncatedInfo.textContent);
+    truncatedInfo.classList.toggle('truncate-me');
+    if (findOutMoreBtn.innerHTML === "more") {
+      findOutMoreBtn.innerHTML = "less"
+    } else {
+      findOutMoreBtn.innerHTML = "more"
+    }
+  })
+
+}
+
+export { collapseBtn }
