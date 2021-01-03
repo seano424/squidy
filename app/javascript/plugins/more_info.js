b4d9993d43bb8bdb57a80d4bb1136d2b4726c391
Sean OReilly <soreilly424@gmail.com>
2021-01-03 08:24:54 -0500

    finally fixed more info btn to work on opening and closing things

--- app/javascript/plugins/more_info.js
+++ app/javascript/plugins/more_info.js
@@ -1,34 +1,44 @@
 const moreInfo = () => {
-  const map = document.getElementById("map");
-  const config = { attributes: true, childList: true, subtree: true };
-
-  const callback = function (mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for (const mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        // console.log('A child node has been added or removed.');
-      } else if (mutation.type === "attributes") {
-        // console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        // target: div.mapboxgl-popup.mapboxgl-popup-anchor-bottom
-        // console.log(mutation.target);
-      }
+  function handleClick() {
+    let collapseId = document.getElementById(`collapse-${this.name}`);
+    let collapseCardId = document.getElementById(`collapseCard-${this.name}`);
+    collapseCardId.classList.toggle("show");
+    if (collapseId.textContent === "See more") {
+      collapseId.textContent = "See less";
+    } else {
+      collapseId.textContent = "See more";
     }
-    const moreInfoBtn = document.querySelector(".more-info-btn");
-    moreInfoBtn.addEventListener("click", () => {
-      let id = moreInfoBtn.name;
-      let collapseId = document.getElementById(`collapse-${id}`);
-      let collapseCardId = document.getElementById(`collapseCard-${id}`);
-      collapseCardId.classList.toggle("show");
-      if (collapseId.textContent === "See more") {
-        collapseId.textContent = "See less";
-      } else {
-        collapseId.textContent = "See more";
-      }
-    });
-  };
+  }
 
-  const observer = new MutationObserver(callback);
-  // observer.observe(map, config);
+  function moveMouse(e) {
+    if (e.target && e.target.id == "more-info-id") {
+      //do something
+      const moreInfoBtn = document.querySelector(".more-info-btn");
+
+      moreInfoBtn.addEventListener("click", handleClick);
+    }
+  }
+
+  document.addEventListener("mouseover", moveMouse);
+  document.addEventListener("mouseout", moveMouse);
 };
 
 export { moreInfo };
+
+// const moreInfo = () => {
+//   let element = document.getElementById("more-info-id");
+//   if (typeof element != "undefined" && element != null) {
+//     // Exists.
+//     console.log('hello world');
+//     const moreInfoBtn = document.querySelector("#more-info-id");
+
+//       function handleClick() {
+//         let collapseId = document.getElementById(`collapse-${this.name}`);
+//         let collapseCardId = document.getElementById(`collapseCard-${this.name}`);
+//         collapseCardId.classList.toggle("show");
+//       }
+//       moreInfoBtn.addEventListener("click", handleClick);
+//     }
+//   };
+
+//   export { moreInfo };
